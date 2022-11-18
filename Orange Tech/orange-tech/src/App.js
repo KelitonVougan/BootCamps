e0bb1234b320328ddb932d8162af802538557950
KelitonVougan <kelitonvougan@gmail.com>
2022-11-17 22:53:04 -0300

    new file:   Orange Tech/orange-tech/.gitignore 	new file:   Orange Tech/orange-tech/README.md 	new file:   Orange Tech/orange-tech/package-lock.json 	new file:   Orange Tech/orange-tech/package.json 	new file:   Orange Tech/orange-tech/public/index.html 	new file:   Orange Tech/orange-tech/src/App.js 	new file:   Orange Tech/orange-tech/src/index.js

---
+++ Orange Tech/orange-tech/src/App.js
@@ -0,0 +1,12 @@
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ola React</h1>
+      <Button title = "TESTE"/>
+    </div>
+  );
+}
+
+export default App;
